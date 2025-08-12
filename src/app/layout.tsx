d4ece8ffd86f688f2c63ff85fdee767c7a108f83
Yi Zhang <y_zh426@outlook.com>
2025-08-12 16:48:27 +0800

    Update overall layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,12 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<div className="flex min-h-screen max-h-screen w-full">
-					<div className="flex flex-col divide-y-2 divide-gray-300">
+				<div className="fixed flex min-h-screen max-h-screen w-screen h-screen">
+					<div className="fixed bg-white h-full flex flex-col divide-y-2 divide-gray-300">
 						<NavLinks />
 						<SettingsLink />
 					</div>
-					<div className="flex flex-col pl-8 pr-8 bg-gray-300 w-full items-center">
+					<div className="flex flex-col pl-8 pr-8 ml-20 bg-gray-300 items-center h-full w-full">
 						<div className="w-full h-full max-w-4xl p-12 bg-gray-200 border-x-6 border-gray-400">
 						{children}
 						</div>
