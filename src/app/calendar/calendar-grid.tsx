b60aa0effe8c0e0c6f491105c5830ed4e6b65d5a
Yi Zhang <y_zh426@outlook.com>
2025-09-19 10:57:46 +0100

    Amend the layout of the calendar

--- src/app/calendar/calendar-grid.tsx
+++ src/app/calendar/calendar-grid.tsx
@@ -13,18 +13,22 @@ export default function CalendarGrid() {
     return (
         <div className="h-full">
             <div className="mb-2 h-1/20">
-                <h2 className="text-xl font-medium text-center">{`${startOfWeekDate.toLocaleDateString()} - ${endOfWeekDate.toLocaleDateString()} (week ${getWeek(startOfWeekDate)})`}</h2>
+                <h2 className="text-xl font-medium text-center">{`${startOfWeekDate.toLocaleDateString()} - ${endOfWeekDate.toLocaleDateString()} (Week ${getWeek(startOfWeekDate)})`}</h2>
             </div>
             <div className="w-full w-max-[1000px] border-2 border-gray-300 rounded-lg bg-gray-100 h-19/20">
-                <div className="flex flex-col justify-between h-full border-r-3 py-5 border-gray-300">
+                <div className="flex flex-col h-full border-gray-300 divide-y-1 divide-black/10">
                     {weekdays.map((day) => (
-                        <div key={day}>
-                            <p className="[writing-mode:vertical-rl] [text-orientation:upright]">{day}</p>
+                        <div className="flex h-1/7" key={day}>
+                            <div className="flex w-6 border-r-2 border-black/10 justify-center items-center">
+                                <p className="[writing-mode:vertical-rl] [text-orientation:upright] font-semibold text-lg">{day}</p>
+                            </div>
+                            <div>
+
+                            </div>
                         </div>
                     ))}
                 </div>
                 <div className="h-full">
-                    
                 </div>
             </div>
         </div>
